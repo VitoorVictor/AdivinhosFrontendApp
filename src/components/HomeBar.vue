74b0fd702ccd76a51a42d7081a359ed230b81742
Vitor Gabriel Blasques Victor <vitorvictorblasq@gmail.com>
2024-06-28 02:24:04 -0300

    First Commit Frontend A divinhos

---
+++ src/components/HomeBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid">
+    <div class=" homebar row p-2 justify-content-center align-items-center">
+      <div class="col-md-4"><img :src="require('/public/img/Logo A divinhos.png')" alt="logo" class="img-fluid rounded"></div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeBar',
+  
+}
+</script>
+
+<style scoped>
+  .row{
+    height: 140px;
+  }
+  .img-fluid{
+    max-height:120px;
+
+  }
+  
+</style>
